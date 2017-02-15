e39afc1427e72a028913cfcfb8c318c3ed73ba88
Igor Ranieri Elland <igor@elland.me>
2017-02-15 16:14:24 +0100

    Removed dead code

--- EtherealCereal/EtherealCereal.h
+++ EtherealCereal/EtherealCereal.h
@@ -1,12 +1,7 @@
 #import <UIKit/UIKit.h>
 
-//! Project version number for EtherealCereal.
 FOUNDATION_EXPORT double EtherealCerealVersionNumber;
-
-//! Project version string for EtherealCereal.
 FOUNDATION_EXPORT const unsigned char EtherealCerealVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <EtherealCereal/PublicHeader.h>
 
 #import <EtherealCereal/EtherWrapper.h>
 #import "keccak.h"
